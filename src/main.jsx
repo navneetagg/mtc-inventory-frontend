c33eb00c547bf074cc9e98857f7f72ea396b3ce9
Navneet Aggarwal <navneet.com122@gmail.com>
2026-05-13 21:32:52 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import axios from 'axios';
 
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '/api';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
